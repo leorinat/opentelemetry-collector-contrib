f58039db08b3f63eec60f5321673e0683737a39d
Leo Ariel Arias <aarias@atlassian.com>
2024-05-03 23:21:10 +1200

    ----

--- exporter/awskinesisexporter/exporter.go
+++ exporter/awskinesisexporter/exporter.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package awskinesisexporter // import "github.com/open-telemetry/opentelemetry-collector-contrib/exporter/awskinesisexporter"
+package awskinesisexporter // import "github.com/leorinat/opentelemetry-collector-contrib/exporter/awskinesisexporter"
 
 import (
 	"context"
